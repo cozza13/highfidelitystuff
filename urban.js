df5b345940b3de94385a4ffcfdf24738b6c8810c
Chris Collins <chris@highfidelity.io>
2016-12-15 21:44:23 -0800

    change

--- urban.js
+++ urban.js
@@ -1,6 +1,6 @@
 (function() {
     var _this = this;
-    var sound = SoundCache.getSound("http://hifi-content.s3.amazonaws.com/ken/Urban/Urban%20Morning%20Downtown%20City%20Traffic%20Streetcar%20Platform%20ST450_ambiX.wav");
+    var sound = SoundCache.getSound("http://hifi-content.s3.amazonaws.com/ken/Urban/Urban%20Morning%20Downtown%20City%20Traffic%20Overhead%20Expressway%20Traffic%20ST450%2001_ambiX.wav");
 
     // this is the version of injector that is visible to your leaveEntity() method
     var injector;

3034b7c0931da99fc5fb1d654829412df031df9a
Chris Collins <chris@highfidelity.io>
2016-12-15 17:02:26 -0800

    Update and rename brahm6.js to brahm7.js

---
+++ winterscene/brahm7.js
@@ -0,0 +1,20 @@
+(function() {
+    var _this = this;
+    var sound = SoundCache.getSound("https://s3.amazonaws.com/converter.tipodean.com/sounds/Brahms_Sym1-ambiX.wav");
+
+    // this is the version of injector that is visible to your leaveEntity() method
+    var injector;
+
+
+    _this.enterEntity = function(entityID) {
+        print('I am insiude');
+        injector = Audio.playSound(sound, { loop: true, volume: 1.0 });
+        injector.restart();
+    };
+  
+    _this.leaveEntity = function(entityID) {
+        print('I am outsidee');
+        Audio.stop(injector);
+        print("Reverb is OFF.");
+    };
+})
